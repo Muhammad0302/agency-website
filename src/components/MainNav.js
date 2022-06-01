36b267a2a45c427976f97e06b694e836d28c511d
Muhammad Ismail <60970076+Muhammad0302@users.noreply.github.com>
2022-06-01 22:56:13 +0500

    changes added

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -2,10 +2,6 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import BottomNavigation from "@mui/material/BottomNavigation";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
-import RestoreIcon from "@mui/icons-material/Restore";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import LocationOnIcon from "@mui/icons-material/LocationOn";
-
 import WhatshotIcon from "@mui/icons-material/Whatshot";
 import MovieIcon from "@mui/icons-material/Movie";
 import LiveTvIcon from "@mui/icons-material/LiveTv";
